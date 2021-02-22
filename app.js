da6ee7b0e493991702775d9a18633a6a1666d1de
samvalvi <samuelvalerin@gmail.com>
2021-02-22 17:00:45 +0000

    test with nested for

---
+++ app.js
@@ -0,0 +1,22 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+let extension = ['.com', '.net', '.us', '.io'];
+
+const domainNameGenerator = (a, b, c, d) => {
+    let matrix = [a, b, c, d];
+    let domain = ''
+
+    for(let i = 0; i < matrix.length; i++){
+        for(let j = 0; j < matrix[i].length; j++){
+            
+        }
+    }
+
+    return domain;
+}
+
+
+
+let result = domainNameGenerator(); 
+console.log(result);
