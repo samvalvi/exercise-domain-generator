e0a2b25efb24bc83ea9c408178a2c0845ce655a0
samvalvi <samuelvalerin@gmail.com>
2021-02-23 03:35:49 +0000

    Final commit

--- app.js
+++ app.js
@@ -4,21 +4,15 @@ let noun = ['jogger','racoon'];
 let extension = ['.com', '.net', '.us', '.io'];
 
 const domainNameGenerator = (a, b, c, d) => {
-    let domain = ''
 
     for (let i = 0; i < a.length; i++)
     {
         for (let j = 0; j < b.length; j++)
         {
-            domain += a[Math.floor(Math.random() * 2)] + b[Math.floor(Math.random() * 2)] + c[Math.floor(Math.random() * 2)] 
-            + d[Math.floor(Math.random() * 4)];
+            console.log(a[Math.floor(Math.random() * 2)] + b[Math.floor(Math.random() * 2)] + c[Math.floor(Math.random() * 2)] 
+            + d[Math.floor(Math.random() * 4)]);
         }
     }
-
-    return domain;
 }
 
-
-
-let result = domainNameGenerator(pronoun, adj, noun, extension); 
-console.log(result);
+domainNameGenerator(pronoun, adj, noun, extension); 
